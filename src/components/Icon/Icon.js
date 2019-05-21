33e8626903e5addcac084af268018a9fafdff4a8
Cliff Pyles <cliff@cliffpyles.com>
2019-05-21 07:51:25 -0400

    Update icons

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import { Icon as PolarisIcon } from '@shopify/polaris'
-import { IconContext } from 'react-icons'
-import { MdExitToApp } from 'react-icons/md'
-import { MdHome } from 'react-icons/md'
-import { MdSchool } from 'react-icons/md'
-import { MdQuestionAnswer } from 'react-icons/md'
-import { MdPlaylistAddCheck } from 'react-icons/md'
+import { ReactComponent as IconicHome } from './img/home.svg'
+import { ReactComponent as IconicAccountLogout } from './img/account-logout.svg'
+import { ReactComponent as IconicLightbulb } from './img/lightbulb.svg'
+import { ReactComponent as IconicQuestionMark } from './img/question-mark.svg'
+import { ReactComponent as IconicFile } from './img/file.svg'
 
-export const LogoutIcon = props => <Icon {...props} source={MdExitToApp} />
-export const HomeIcon = props => <Icon {...props} source={MdHome} />
-export const CoursesIcon = props => <Icon {...props} source={MdSchool} />
-export const QuestionsIcon = props => (
-  <Icon {...props} source={MdQuestionAnswer} />
-)
-export const QuizzesIcon = props => (
-  <Icon {...props} source={MdPlaylistAddCheck} />
-)
+import './Icon.css'
 
 export const Icon = ({ source, ...props }) => {
-  return (
-    <IconContext.Provider value={{ size: '2rem' }}>
-      <PolarisIcon {...props} source={source()} />
-    </IconContext.Provider>
-  )
+  return <PolarisIcon source={source} {...props} />
 }
+export const LogoutIcon = props => (
+  <Icon {...props} source={<IconicAccountLogout />} />
+)
+export const HomeIcon = props => <Icon {...props} source={<IconicHome />} />
+export const CoursesIcon = props => (
+  <Icon {...props} source={<IconicLightbulb />} />
+)
+export const QuestionsIcon = props => (
+  <Icon {...props} source={<IconicQuestionMark />} />
+)
+export const QuizzesIcon = props => <Icon {...props} source={<IconicFile />} />
